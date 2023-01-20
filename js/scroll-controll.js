55d125d34d20a773099da0455beafddcdd9b99e5
Victor Diaz <master_v12@hotmail.com>
2023-01-19 22:14:32 -0700

    experimented with scroll, will return later

--- js/scroll-controll.js
+++ js/scroll-controll.js
@@ -7,16 +7,16 @@ const checkThrottle = throttle(evt => {})
 // add scroll event to all elements with 'card' class
 var cards = document.getElementsByClassName("card");
 for (let i = 0; i < cards.length; i++){
-    cards[i].addEventListener("scroll", evt => {
+    cards[i].addEventListener("click", evt => {
         //pass event to checkThrottle for callback & store
         //shouldWait parameter to prevent default if true
         let holdScroll = checkThrottle(evt)
-        if(holdScroll) evt.preventDefault()
+        //if(holdScroll) evt.preventDefault()
     });
 }
 
 //throttle function
-function throttle(cb, delay =  200, evt) {
+function throttle(cb, delay =  500, evt) {
     let shouldWait = false
     return (...args) => {
         //once should wait is true, will return it to pause scroll overlap
@@ -31,3 +31,11 @@ function throttle(cb, delay =  200, evt) {
         }, delay)
     }
 }
+
+//  document.querySelector(".container").addEventListener("wheel", preventScroll, {passive: false});
+
+//  function preventScroll(e){
+//     e.preventDefault();
+//     e.stopPropagation();
+//     return false;
+//  }
