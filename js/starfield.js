d79aecaee8a200cc4b0b301ddb58d3fa98b607a4
Victor Diaz <master_v12@hotmail.com>
2023-01-28 00:40:38 -0700

    added second layer to scrolling background

--- js/starfield.js
+++ js/starfield.js
@@ -6,9 +6,9 @@ function getCoordinates(sfWidth, sfHeight) {
     return [x, y];
 }
 
-function getStarField(sfWidth, sfHeight) {
+function getStarField(sfWidth, sfHeight, starcount) {
     let field = " 1px 1px #fff";
-    for (let i = 0; i < 200; i++){
+    for (let i = 0; i < starcount; i++){
         let [x, y] = getCoordinates(sfWidth, sfHeight);
         field += ", " + x.toString() + "px " + y.toString() + "px #fff";
     }
@@ -17,8 +17,11 @@ function getStarField(sfWidth, sfHeight) {
 
 document.addEventListener("DOMContentLoaded", function(){
     const sf = document.getElementById("starfield");
-    const container = document.getElementById("contentField");
-    let sfWidth = container.offsetWidth;
-    let sfHeight = container.offsetHeight;
-    sf.style.boxShadow = getStarField(sfWidth, sfHeight);
+    const sf2 = document.getElementById("starfield2");
+    const containerDiv = document.getElementById("containerField");
+    const contentDiv = document.getElementById("contentField");
+    let sfWidth = containerDiv.offsetWidth;
+    let sfHeight = contentDiv.offsetHeight;
+    sf.style.boxShadow = getStarField(sfWidth, sfHeight / 5, 200);
+    sf2.style.boxShadow = getStarField(sfWidth, sfHeight / 3.5, 50);
 });
