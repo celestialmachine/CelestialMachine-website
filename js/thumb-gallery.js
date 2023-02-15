abb205e5aece8c529a4f29f69336c7ff2d514217
Victor Diaz <master_v12@hotmail.com>
2023-02-14 20:09:29 -0700

    added hover functionality to thumbnail gallery

--- js/thumb-gallery.js
+++ js/thumb-gallery.js
@@ -6,17 +6,28 @@ const linkThumbnails = (gallery) => {
     //get previous sibling(project-image)
     let mainImage = gallery.previousElementSibling;
     const thumbnails = gallery.querySelectorAll("a");
+    //add grown class to first element on content load
+    thumbnails[0].firstElementChild.classList.add("grown");
     //process thumbnails
-    for(let thumbnail of thumbnails) {
+    for(let i = 0; i < thumbnails.length; i++){
         //preload image
         const image = new Image();
-        image.src = thumbnail.href;
-        //event handler for thumbnail
-        thumbnail.addEventListener("click", evt =>{
-            mainImage.src = thumbnail.href;
+        image.src = thumbnails[i].href;
+        //event handler for thumbnails
+        thumbnails[i].addEventListener("click", evt =>{
+            mainImage.src = thumbnails[i].href;
             //TODO add alt
             mainImage.alt = "";
+            //add 'grown' class to first child of 'a' which is the 'img'
+            thumbnails[i].firstElementChild.classList.add("grown");
+            //prevent default behavior: opening img in new window/tab
             evt.preventDefault();
+            //remove grow class from all 'img' elements except the one clicked on
+            for(let j = 0; j < thumbnails.length; j++){
+                if(j != i){
+                    thumbnails[j].firstElementChild.classList.remove("grown");
+                }
+            }
         });
     }
 }
