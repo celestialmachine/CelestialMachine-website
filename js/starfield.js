53caa11f8382d0c0bb1e34fc69217d8c7189428f
Victor Diaz <master_v12@hotmail.com>
2023-02-23 19:43:12 -0700

    fixed start field, best as I could lol

--- js/starfield.js
+++ js/starfield.js
@@ -4,7 +4,7 @@ function getCoordinates(sfWidth, sfHeight) {
     let x = Math.floor(Math.random() * sfWidth);
     let y = Math.floor(Math.random() * sfHeight);
     return [x, y];
-}
+};
 
 function getStarField(sfWidth, sfHeight, starCount) {
     let field = " 1px 1px #fff";
@@ -15,14 +15,23 @@ function getStarField(sfWidth, sfHeight, starCount) {
     return field;
 };
 
+function longestSide() {
+    if (screen.height > screen.width){
+        return screen.height;
+    }
+    else {
+        return screen.width;
+    }
+};
+
 document.addEventListener("DOMContentLoaded", function(){
     const sf1 = document.getElementById("starfield1");
     const sf2 = document.getElementById("starfield2");
     const sf3 = document.getElementById("starfield3");
     const contentDiv = document.getElementById("contentField");
-    let sfWidth = screen.width;
+    let sfWidth = longestSide();
     let sfHeight = contentDiv.offsetHeight;
-    sf1.style.boxShadow = getStarField(sfWidth / 2.5, sfHeight / 5, 200);
-    sf2.style.boxShadow = getStarField(sfWidth, sfHeight / 3.5, 80);
+    sf1.style.boxShadow = getStarField(sfWidth, sfHeight /4, 200);
+    sf2.style.boxShadow = getStarField(sfWidth, sfHeight / 3, 80);
     sf3.style.boxShadow = getStarField(sfWidth, sfHeight, 30)
 });
